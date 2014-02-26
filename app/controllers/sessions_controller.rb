6be7a1fada7bdd57f11647f4806f3dfdbc71b9eb
Errol Siegel <errolsiegel@gmail.com>
2014-02-26 17:45:04 -0600

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-  protect_from_forgery with: :exception
   include SessionsHelper
 
   def new
@@ -9,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
