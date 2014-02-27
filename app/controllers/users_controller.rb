74d7f6f2ea8d19fdf009f3fb1bedbfced530269f
Errol Siegel <errolsiegel@gmail.com>
2014-02-27 14:03:01 -0600

    Updates from exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+  before_action :not_signed_in_user, only: [:new, :create]
   include SessionsHelper
 
   def index
@@ -40,8 +41,13 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
+    user = User.find(params[:id])
+    if current_user?(user) && user.admin?
+      flash[:error] = "You cannot delete yourself."
+    else
+      user.destroy
+      flash[:success] = "User deleted."
+    end
     redirect_to users_url
   end
 
@@ -59,6 +65,10 @@ class UsersController < ApplicationController
         store_location
         redirect_to signin_url, notice: "Please sign in."
       end
+    end
+
+    def not_signed_in_user
+      redirect_to(root_url) if signed_in?
     end
 
     def correct_user
